bc170d25ede7ae9f6db87ed6e7940de403803f98
jar00t <myn.fajar@gmail.com>
2018-11-23 14:48:10 +0700

    first commit

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -75,18 +75,18 @@ public final class R {
 
         public static final int bottom = 0x7f080023;
         public static final int end = 0x7f080042;
-        public static final int gone = 0x7f080051;
-        public static final int invisible = 0x7f08005e;
-        public static final int left = 0x7f080065;
-        public static final int packed = 0x7f08007e;
-        public static final int parent = 0x7f080080;
-        public static final int percent = 0x7f080083;
-        public static final int right = 0x7f080088;
-        public static final int spread = 0x7f0800af;
-        public static final int spread_inside = 0x7f0800b0;
-        public static final int start = 0x7f0800b5;
-        public static final int top = 0x7f0800cc;
-        public static final int wrap = 0x7f0800e3;
+        public static final int gone = 0x7f080052;
+        public static final int invisible = 0x7f08005f;
+        public static final int left = 0x7f080068;
+        public static final int packed = 0x7f080081;
+        public static final int parent = 0x7f080083;
+        public static final int percent = 0x7f080086;
+        public static final int right = 0x7f08008b;
+        public static final int spread = 0x7f0800b2;
+        public static final int spread_inside = 0x7f0800b3;
+        public static final int start = 0x7f0800b8;
+        public static final int top = 0x7f0800cf;
+        public static final int wrap = 0x7f0800e6;
     }
     public static final class styleable {
         private styleable() {}
--- app/src/main/java/id/inharmonia/app/SearchActivity.java
+++ app/src/main/java/id/inharmonia/app/SearchActivity.java
@@ -2,13 +2,61 @@ package id.inharmonia.app;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class SearchActivity extends AppCompatActivity {
+
+    ImageView mBackButton;
+    ImageView mClearButton;
+    EditText mSearchInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        mBackButton = findViewById(R.id.ivBackButton);
+        mClearButton = findViewById(R.id.ivClearButton);
+        mSearchInput = findViewById(R.id.etSearchInput);
+
+        mBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        mSearchInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (mSearchInput.getText().toString().matches("")) {
+                    mClearButton.setVisibility(View.INVISIBLE);
+                } else {
+                    mClearButton.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        mClearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSearchInput.setText("");
+            }
+        });
     }
 
 }
