1c9acdac0af7f43ee965b83aecef0e45499c6dfb
jar00t <myn.fajar@gmail.com>
2018-12-05 16:33:51 +0700

    first commit

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -74,19 +74,19 @@ public final class R {
         private id() {}
 
         public static final int bottom = 0x7f080023;
-        public static final int end = 0x7f080044;
-        public static final int gone = 0x7f080055;
-        public static final int invisible = 0x7f080067;
-        public static final int left = 0x7f080070;
-        public static final int packed = 0x7f08008c;
-        public static final int parent = 0x7f08008e;
-        public static final int percent = 0x7f080091;
-        public static final int right = 0x7f080096;
-        public static final int spread = 0x7f0800c0;
-        public static final int spread_inside = 0x7f0800c1;
-        public static final int start = 0x7f0800c6;
-        public static final int top = 0x7f0800dc;
-        public static final int wrap = 0x7f0800fd;
+        public static final int end = 0x7f080045;
+        public static final int gone = 0x7f080056;
+        public static final int invisible = 0x7f080068;
+        public static final int left = 0x7f080071;
+        public static final int packed = 0x7f08008d;
+        public static final int parent = 0x7f08008f;
+        public static final int percent = 0x7f080092;
+        public static final int right = 0x7f080097;
+        public static final int spread = 0x7f0800c1;
+        public static final int spread_inside = 0x7f0800c2;
+        public static final int start = 0x7f0800c7;
+        public static final int top = 0x7f0800dd;
+        public static final int wrap = 0x7f0800fe;
     }
     public static final class styleable {
         private styleable() {}
--- app/src/main/java/id/inharmonia/app/Home/SizeQuantityPopup.java
+++ app/src/main/java/id/inharmonia/app/Home/SizeQuantityPopup.java
@@ -6,10 +6,14 @@ import android.support.design.widget.BottomSheetDialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import id.inharmonia.app.R;
 
 public class SizeQuantityPopup extends BottomSheetDialogFragment {
+
+    Button mAddToCartButton;
+
     public SizeQuantityPopup() {
 
     }
@@ -22,6 +26,18 @@ public class SizeQuantityPopup extends BottomSheetDialogFragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.popup_size_quantity, container, false);
+
+        View view = inflater.inflate(R.layout.popup_size_quantity, container, false);
+
+        mAddToCartButton = view.findViewById(R.id.btAddToCart);
+        mAddToCartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+        return view;
     }
 }
